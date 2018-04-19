a9653c3d5122e954de5fdda0840127c63a829f29
Alistair Parkinson <37090819+1011776@users.noreply.github.com>
2018-04-19 12:19:25 +0800

    We fixed the code for the CO2 Sensor.
    Also fixed the code for temp sensor and other stuff. :-)

--- arduino_robot_read_sensors.ino
+++ arduino_robot_read_sensors.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial co2Serial(10, 11); // RX, TX
 unsigned char hexdata[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
 char CO2 = true;
+char temperature = false;
 
 // This is the functions that reads the pin's values, 
 // and outputs the pin number and analog value from the sensor.
@@ -9,11 +10,13 @@ void readPin(int pin){
   Serial.print(pin); 
   Serial.println(analogRead(pin));
 }
-void readCO2() {} {
-    for(int i=0,j=0;i<9;i++)  {
-      if (mySerial.available()>0) {
+
+void readCO2() {
+    co2Serial.write(hexdata,9);
+    for(int i=0;i<9;i++)  {
+      if (co2Serial.available()>0) {
         long hi,lo,CO2;
-        int ch=mySerial.read();
+        int ch=co2Serial.read();
     
         if(i==2){
           hi=ch; //High concentration
@@ -27,22 +30,19 @@ void readCO2() {} {
           Serial.println(CO2);
         }
       }
-    }
+    } 
   }
 // The setup of the Serial communication, on channel 9600.
 void setup(){
   Serial.begin(9600);
+  co2Serial.begin(9600);
 }
 // This is the code that repeats every 0.5 seconds.
 void loop(){
-  // This 'for' loop is used to see what pins are recieving a analog signal. 
-  // If they are, they will output this signal to the serial communication with the "readPin(x)" function.
-  for(int i = 1; i <= 7; i++) {
-    if(analogRead(i) != 'None') {
-      readPin(i);
-    }
-  }
+  // They will output this signal to the serial communication with the "readPin(x)" function.
+  if(temperature == true) {readPin(2);}
   if(CO2 == true) {readCO2();}
+
   // A delay of 500 milliseconds (0.5 seconds) is used to stop python from reading the values too quickly. 
   // Change to 250 milliseconds if absolutely neccessary.
   delay(500);
