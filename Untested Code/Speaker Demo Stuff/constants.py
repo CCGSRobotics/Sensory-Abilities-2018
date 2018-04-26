2bc8dd88437e415e6d32212b118b5a11493f675f
Callum Koh <euguankoh@My-Spaghet.local>
2018-04-26 15:25:39 +0800

    Here's the speaker code

---
+++ Untested Code/Speaker Demo Stuff/constants.py
@@ -0,0 +1,40 @@
+# TCP Constants
+import pyaudio
+
+RATE = 48000
+CHANNELS = 2
+CHUNK_SIZE = 1024
+WIDTH = pyaudio.paInt32
+
+HOST = "192.168.100.1"
+PORT = 12024
+
+class AudioPlayer:
+    """ Plays a single chunk of audio data """ 
+    def __init__(self, chunksize, width, rate, channels):
+        """ Init audio stream """ 
+        self.p = pyaudio.PyAudio()
+
+        self.audioData = "" # This is mutable
+        self.chunksize = chunksize
+        self.width = width
+        self.rate = rate
+        self.channels = channels
+
+        self.stream = self.p.open(
+            format = self.width,
+            channels = self.channels,
+            rate = self.rate,
+            output = True
+        )
+
+    def play(self):
+        """ Play entire BYTE STRING """
+        for i in range(0,len(self.audioData),len(self.audioData)//self.chunksize):
+            #print(self.audioData[i:i + (len(self.audioData)//self.chunksize)])
+            self.stream.write(self.audioData[i:i + (len(self.audioData)//self.chunksize)])
+
+    def close(self):
+        """ Graceful shutdown """ 
+        self.stream.close()
+        self.p.terminate()
---
+++ Untested Code/Speaker Demo Stuff/pyaudioServer.py
@@ -0,0 +1,26 @@
+# pyaudio callback server
+
+import pyaudio, time, socket
+from constants import *
+
+player = AudioPlayer(CHUNK_SIZE, WIDTH, RATE, CHANNELS)
+
+s = socket.socket()
+s.bind((HOST,PORT))
+
+s.listen(1)
+client, address = s.accept()
+
+print("Connection from: " + str(address))
+
+while True:
+    data = client.recv(8192)
+    if not data:
+        break
+    
+    #print("From connected user: " + data)
+    player.audioData = data
+    player.play()
+
+client.close()
+player.close()
