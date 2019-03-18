af38b04baf328b44f1caf0def27b661dc4a84613
Namor888 <37795234+Namor888@users.noreply.github.com>
2019-03-18 21:00:59 +0800

    Edited Training Code.py

--- Hazmat/TensorflowWork/Training Code.py
+++ Hazmat/TensorflowWork/Training Code.py
@@ -3,3 +3,5 @@ model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("Hazmat Training")
 model_trainer.trainModel(num_objects=4, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
+
+#Algorithms for image prediction include SqueezeNet, ResNet, InceptionV3, DenseNet
