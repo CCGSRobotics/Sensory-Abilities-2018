f24a564dc04219486dfcb5bdd9c316d044c0f81d
Namor888 <37795234+Namor888@users.noreply.github.com>
2019-03-18 21:17:18 +0800

    Fixed Stupid Mistake

--- Hazmat/TensorflowWork/Training Code.py
+++ Hazmat/TensorflowWork/Training Code.py
@@ -2,6 +2,6 @@ from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("Hazmat Training")
-model_trainer.trainModel(num_objects=4, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=61, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
 
 #Algorithms for image prediction include SqueezeNet, ResNet, InceptionV3, DenseNet
